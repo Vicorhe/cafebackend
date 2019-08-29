b9316fec96d893d4bfb729e29c15b75f719f304f
Victor He <vctr.he@gmail.com>
2019-08-30 02:30:05 +0800

    get access token and maintain local copy

---
+++ cloudfunctions/getaccesstoken/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const rp = require('request-promise');
+cloud.init()
+
+const APPID = 'wx6a3c4fc28fb3d43b';
+const APPSECRET = '077f2ca5b12f1a3b476130ec405bcd4c';
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let res = await rp({
+    url: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${APPSECRET}`,
+    method: 'GET'
+  });
+  let xml = res.toString("utf-8");
+  const response = JSON.parse(res);
+  return {
+    response
+  }
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,12 +5,20 @@ Page({
    * Page initial data
    */
   data: {
+    access_token: '',
+    expires_time: 0,
+    products: [],
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function () {
+    this.getValidAccessToken();
+  },
+
+  onShow: function () {
+    this.getValidAccessToken();
   },
 
   onTapMemberUpgrade: function () {
@@ -42,6 +50,28 @@ Page({
       });
     })
     .catch(err => console.log(err))
+  },
+
+  getValidAccessToken: function () {
+    const now = Date.now();
+    if (this.data.expires_time < now){
+      console.log('new access token');
+      wx.cloud.callFunction({
+        name: "getaccesstoken",
+        data: {}
+      })
+      .then(res => {
+        this.setData({
+          access_token: res.result.response.access_token,
+          expires_time: res.result.response.expires_in * 761 + Date.now()
+        });
+      })
+      .catch(console.log);
+    }
+  },
+
+  temp:function () {
+    this.getValidAccessToken();
   }
 
 })

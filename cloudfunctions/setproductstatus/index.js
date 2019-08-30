2a338503c0bc3f5fc1d11514fccd0618a25dfc23
Victor He <vctr.he@gmail.com>
2019-08-31 01:22:07 +0800

    refactored set product status

--- cloudfunctions/setproductstatus/index.js
+++ cloudfunctions/setproductstatus/index.js
@@ -1,20 +1,19 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
 const rp = require('request-promise');
+
 cloud.init()
 
-// 云函数入口函数
 exports.main = async (event, context) => {
   const ACCESS_TOKEN = event.access_token;
-  const productID = event.id;
-  const newStatus = event.status;
+  const PRODUCT_ID = event.product_id;
+  const NEW_STATUS = event.new_status;
 
   let res = await rp({
     url: `https://api.weixin.qq.com/tcb/databaseupdate?access_token=${ACCESS_TOKEN}`,
     method: 'POST',
     body: {
       env: "minicafeenv",
-      query: `db.collection(\"products\").doc(\"${productID}\").update({data:{in_stock: ${newStatus}}})`
+      query: `db.collection(\"products\").doc(\"${PRODUCT_ID}\").update({data:{in_stock: ${NEW_STATUS}}})`
     },
     json: true
   });
